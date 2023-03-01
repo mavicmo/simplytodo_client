747da70f99309c2600b67b8a221b571db134223b
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-02-28 23:21:45 -0500

    set up folders and create login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home";
+import Login from "./pages/login";
+import ProfilePage from "./pages/profile";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/todo" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
