33e2d63f0ec88f36f7a25b270d7a307a7de548bd
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-03-02 08:50:40 -0500

    start on the homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>index</div>;
+  return (
+    /* Global Container */
+    <div className="flex items-center justify-center min-h-screen bg-teal-50">
+      {/* Card Container */}
+      <div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0"></div>
+    </div>
+  );
 };
 
 export default HomePage;
