e26aac7d34e593231c28da322439b4bbdb57ce41
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-03-02 08:46:07 -0500

    finsih login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const URL = "http://localhost:3005/";
 
 const Login = () => {
   const [isRegister, setIsRegister] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [isEmailExist, setIsEmailExist] = useState(false);
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -20,11 +25,29 @@ const Login = () => {
   });
 
   const formRegisterSubmit = async (data) => {
-    const response = await axios.post(URL + `users/register`);
+    try {
+      await axios.post(URL + `users/register`, data);
+      setIsRegistered(true);
+      setIsEmailExist(false);
+      reset();
+    } catch (error) {
+      console.log(error);
+      setIsEmailExist(true);
+      setIsRegistered(false);
+    }
   };
 
-  const formLoginSubmit = (data) => {
-    console.log(data);
+  const formLoginSubmit = async (data) => {
+    try {
+      const { email, password } = data;
+      const payload = { email, password };
+      const res = await axios.post(URL + `users/login`, payload);
+      localStorage.setItem("user", JSON.stringify(res.data));
+      console.log(res.data);
+      navigate("/todo");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -130,6 +153,50 @@ const Login = () => {
                   <p className="text-red-500 font-bold ml-2">
                     {errors.password?.message}
                   </p>
+                  {isRegistered ? (
+                    <div
+                      class="bg-teal-100 m-2 item-center justify-center w-full border rounded-lg border-green-400 text-teal-700 px-4 py-3 gap-1  relative"
+                      role="alert"
+                    >
+                      <strong class="font-bold">Success! </strong>
+                      <span class="block sm:inline">
+                        Your account has been created!
+                      </span>
+                      <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg
+                          class="fill-current h-6 w-6 text-green-500"
+                          role="button"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <title>Close</title>
+                          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                        </svg>
+                      </span>
+                    </div>
+                  ) : null}
+                  {isEmailExist ? (
+                    <div
+                      class="bg-red-100 m-2 border item-center justify-center border-red-400 text-red-700 px-4 py-3 rounded relative"
+                      role="alert"
+                    >
+                      <strong class="font-bold">Error! </strong>
+                      <span class="block sm:inline">
+                        Email already Exist! Please try new Email!
+                      </span>
+                      <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg
+                          class="fill-current h-6 w-6 text-red-500"
+                          role="button"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <title>Close</title>
+                          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                        </svg>
+                      </span>
+                    </div>
+                  ) : null}
                   {/* Border */}
                   <div className="mt-12 border-b border-b-gray-300"></div>
                   {/* bottom content */}
